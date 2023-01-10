34418620de0cf5ce661f40a80b3a01afe4e2c3ca
Iman N <manznurz@gmail.com>
2023-01-10 15:13:26 +0700

    update HomeActivity

--- app/src/main/java/com/imanancin/mymoviedb/ui/home/HomeActivity.kt
+++ app/src/main/java/com/imanancin/mymoviedb/ui/home/HomeActivity.kt
@@ -1,11 +1,6 @@
 package com.imanancin.mymoviedb.ui.home
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,33 +11,6 @@ import com.imanancin.mymoviedb.R
 
 class HomeActivity : AppCompatActivity() {
 
-    private lateinit var broadcastReceiver: BroadcastReceiver
-
-    private fun registerBroadCastReceiver() {
-        broadcastReceiver = object : BroadcastReceiver() {
-            override fun onReceive(context: Context, intent: Intent) {
-                when (intent.action) {
-                    Intent.ACTION_POWER_CONNECTED -> {
-                        Toast.makeText(this@HomeActivity, "Baterai konek", Toast.LENGTH_SHORT).show()
-                    }
-                    Intent.ACTION_POWER_DISCONNECTED -> {
-                        Toast.makeText(this@HomeActivity, "Baterai dicabut", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-        val intentFilter = IntentFilter()
-        intentFilter.apply {
-            addAction(Intent.ACTION_POWER_CONNECTED)
-            addAction(Intent.ACTION_POWER_DISCONNECTED)
-        }
-        registerReceiver(broadcastReceiver, intentFilter)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        registerBroadCastReceiver()
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
